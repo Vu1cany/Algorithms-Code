ab38c67a2b00f407117aafff615351ff36511015
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2022-12-15 11:56:13 +0300

    RadixSort was added

---
+++ RadixSort.cpp
@@ -0,0 +1,39 @@
+//n - количество сортируемых строк
+//m - длина строк (одинаковая)
+//k - колличество сортируемых разрядов (считать справа)
+
+std::string* RadixSort(std::string *ArrString, int n, int m, int k) {
+    for (int i = 0; i < k; ++i) {
+        std::string s1;
+        for (int j = 0; j < n; ++j) {
+            s1 += ArrString[j][m - 1 - i];
+        }
+
+        int count = -INT_MAX;
+        for (char C: s1) {
+            count = std::max((int)C + 1, count);
+        }
+
+        int CountArr[count];
+        for (int j = 0; j < count; ++j) CountArr[j] = 0;
+
+        for (char C: s1) {
+            CountArr[(int)C] = ++CountArr[(int)C];
+        }
+
+        for (int j = 1; j < count; ++j) {
+            CountArr[j] = CountArr[j] + CountArr[j-1];
+        }
+
+        std::string stringArrSort[n];
+        for (int j = n - 1; j >= 0; --j) {
+            if (CountArr[(int)ArrString[j][m - 1 - i]] != 0) {
+                stringArrSort[CountArr[(int)ArrString[j][m - 1 - i]] - 1] = ArrString[j];
+                CountArr[(int)ArrString[j][m - 1 - i]]--;
+            }
+        }
+        for (int j = 0; j < n; ++j) ArrString[j] = stringArrSort[j];
+    }
+
+    return ArrString;
+}
